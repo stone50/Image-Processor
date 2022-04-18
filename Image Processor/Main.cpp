75dc9ede6f8c0fefe314aae33355f6c1d05c1e45
Stone Hobbs <stone@minnykid.com>
2022-04-18 17:26:48 -0500

    Row and col swap fix

--- Image Processor/Main.cpp
+++ Image Processor/Main.cpp
@@ -1,18 +1,28 @@
-#include <iostream>
+#include<windows.h>
+#include<iostream>
 
 #include "Image.h"
 
 using namespace std;
 
 int main() {
-	string fileName("bitmap.bmp");
-	Image bmp;
-	bool success = bmp.load(fileName);
-	if (success) {
-		cout << fileName << " loaded successfully!" << endl;
-	}
-	else {
-		cout << fileName << " did not load" << endl;
-	}
-	return 1;
+    Image img("bitmap.bmp");
+
+    //Get a console handle
+    HWND myconsole = GetConsoleWindow();
+
+    //Get a handle to device context
+    HDC mydc = GetDC(myconsole);
+
+    //Draw pixels
+    for (unsigned int row = 0; row < img.getHeight(); row++) {
+        for (unsigned int col = 0; col < img.getWidth(); col++) {
+            SetPixel(mydc, col, row, RGB(img.getPixel(col, row).red, img.getPixel(col, row).green, img.getPixel(col, row).blue));
+        }
+    }
+
+    img.~Image();
+    ReleaseDC(myconsole, mydc);
+    cin.ignore();
+    return 0;
 }
